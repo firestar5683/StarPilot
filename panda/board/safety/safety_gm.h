0c1833d51415e085cf6e036f71c5d5415dc9cc40
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-02-10 16:05:30 -0600

    Update safety_gm.h

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -10,21 +10,21 @@ const SteeringLimits GM_STEERING_LIMITS = {
 };
 
 const LongitudinalLimits GM_ASCM_LONG_LIMITS = {
-  .max_gas = 7168,
-  .min_gas = 5500,
-  .inactive_gas = 5500,
+  .max_gas = 8848,
+  .min_gas = 5610,
+  .inactive_gas = 5650,
   .max_brake = 400,
 };
 
 const LongitudinalLimits GM_ASCM_LONG_LIMITS_SPORT = {
-  .max_gas = 8191,
+  .max_gas = 8848,
   .min_gas = 5500,
-  .inactive_gas = 5500,
+  .inactive_gas = 5650,
   .max_brake = 400,
 };
 
 const LongitudinalLimits GM_CAM_LONG_LIMITS = {
-  .max_gas = 7496,
+  .max_gas = 8848,
   .min_gas = 5610,
   .inactive_gas = 5650,
   .max_brake = 400,
