ee80cdbfe4203b2816de7f8984d31390e5437377
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2026-04-05 17:08:05 -0500

    mapbox

--- starpilot/system/the_pond/assets/components/sidebar.js
+++ starpilot/system/the_pond/assets/components/sidebar.js
@@ -16,6 +16,7 @@ const MENU_ITEMS = {
     { name: "Galaxy", link: "/galaxy", icon: "bi-globe2" },
     { name: "Long Maneuvers", link: "/longitudinal_maneuvers", icon: "bi-signpost-split" },
     { name: "Maps", link: "/manage_maps", icon: "bi-map" },
+    { name: "Navigation Keys", link: "/manage_navigation_keys", icon: "bi-key-fill" },
     { name: "Model Manager", link: "/manage_models", icon: "bi-cpu" },
     { name: "Plots", link: "/plots", icon: "bi-graph-up-arrow" },
     { name: "Testing Ground", link: "/testing_ground", icon: "bi-bezier2" },
